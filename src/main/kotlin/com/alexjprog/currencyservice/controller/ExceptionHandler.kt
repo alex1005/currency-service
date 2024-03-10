519929929f1c717e3b3e2cb62a06a2177d11a1ec
Alex Chemanov <alexjprog@gmail.com>
2024-03-10 15:06:22 +0200

    add: MethodArgumentTypeMismatchException handling

--- src/main/kotlin/com/alexjprog/currencyservice/controller/ExceptionHandler.kt
+++ src/main/kotlin/com/alexjprog/currencyservice/controller/ExceptionHandler.kt
@@ -9,12 +9,13 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 
 @RestControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(WrongAmountException::class, WrongBaseCurrencyException::class, WrongCurrenciesException::class,
-        MissingServletRequestParameterException::class)
+        MissingServletRequestParameterException::class, MethodArgumentTypeMismatchException::class)
     fun handleException(ex: Exception): ResponseEntity<ErrorResponse> {
         return ResponseEntity(ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.message.toString()), HttpStatus.BAD_REQUEST)
     }
